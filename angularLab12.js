633c513bf6a696c61622a15aa5439ffcde4377ff
Matt Naif <99man85@gmail.com>
2016-09-24 22:02:17 -0400

    Update angularLab12.js

--- angularLab12.js
+++ angularLab12.js
@@ -1,5 +1,5 @@
 var app = angular.module('myModule', []);
-// This is declaring a module. More on this in a moment
+// This is declaring a module.
 app.controller('myController', function($scope){
 $scope.toDo = ['Grocery Shopping', 'Clean', 'Take out Garbage', 'Make Bed'];
 $scope.addToDo = function(input){
